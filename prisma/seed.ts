36a745949b007f0fcd3c42548bfb9a672e214fc9
Karina Barinova <kbarinova11@gmail.com>
2022-11-30 20:48:42 +0000

    Add playlist seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -34,6 +34,23 @@ const run = async () => {
             name: 'John Doe'
         }
     })
+
+    const songs = await prisma.song.findMany({}) //all songs
+    await Promise.all(new Array(10).fill(1).map(async (_, i) => {
+        return prisma.playlist.create({
+            data: {
+                name: `Playlist ${i + 1}`,
+                user: {
+                    connect: { id: user.id }
+                },
+                songs: {
+                    connect: songs.map((song) => ({
+                        id: song.id
+                    }))
+                }
+            }
+        })
+    }))
 }
 
 run()
