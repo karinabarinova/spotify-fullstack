dbd684007b3b28b369a4e0c52dcac7d5b76ba8da
Karina Barinova <kbarinova11@gmail.com>
2022-11-30 17:45:31 +0000

    Add navigation menu items

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,25 @@
 import NextImage from "next/image"
+import NextLink from "next/link"
 import { Box, List, ListItem, ListIcon, Divider, Center, LinkBox, LinkOverlay } from "@chakra-ui/layout"
 import { MdHome, MdSearch, MdLibraryMusic, MdPlaylistAdd, MdFavorite } from 'react-icons/md'
+
+const navMenu = [
+    {
+        name: "Home",
+        icon: MdHome,
+        route: "/"
+    },
+    {
+        name: "Search",
+        icon: MdSearch,
+        route: "search"
+    },
+    {
+        name: "Your Library",
+        icon: MdLibraryMusic,
+        route: "/library"
+    },
+]
 
 export const Sidebar = () => {
     return (
@@ -9,7 +28,23 @@ export const Sidebar = () => {
                 <Box width="120px" marginBottom="20px" paddingX="20px">
                     <NextImage src="/logo.svg" height={60} width={120} />
                 </Box>
+                <Box marginBottom="20px">
+                    <List spacing={2}>
+                        {navMenu.map(({ name, icon, route }) => (
+                            <ListItem paddingX="20px" fontSize="16px" key={name}>
+                                <LinkBox>
+                                    <NextLink href={route} passHref>
+                                        <LinkOverlay>
+                                            <ListIcon as={icon} color="white" marginRight="20px" />
+                                            {name}
+                                        </LinkOverlay>
+                                    </NextLink>
+                                </LinkBox>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
             </Box>
-        </Box>
+        </Box >
     )
 }
