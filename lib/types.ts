27da75d43fdb6a51328732b318ad0b2f6db00a4c
Karina Barinova <kbarinova11@gmail.com>
2022-12-01 17:54:17 +0000

    Add schema typescript types

---
+++ lib/types.ts
@@ -0,0 +1,39 @@
+export type User = {
+    id: any
+    createdAt: Date
+    updatedAt: Date
+    email: string
+    password: string
+    name: string
+    playlists: Playlist[]
+}
+
+export type Playlist = {
+    id: any
+    createdAt: Date
+    updatedAt: Date
+    name: string
+    songs: any[]
+    user: User
+    userId: string
+}
+
+export type Song = {
+    id: any
+    createdAt: Date
+    updatedAt: Date
+    name: string
+    artist: Artist
+    artistId: string
+    playlists: Playlist[]
+    duration: string
+    url: string
+}
+
+export type Artist = {
+    id: any
+    createdAt: Date
+    updatedAt: Date
+    name: string
+    songs: Song[]
+}
