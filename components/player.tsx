e1606a6a35bce33410fc08f3d554f83accaad904
Karina Barinova <kbarinova11@gmail.com>
2022-12-03 16:35:08 +0000

    Fix song end and repeat functionality

--- components/player.tsx
+++ components/player.tsx
@@ -25,13 +25,15 @@ import { formatTime } from '../lib/formatters'
 
 export const Player = ({ songs, activeSong }) => {
     const [playing, setPlaying] = useState(true)
-    const [index, setIndex] = useState(0)
-    const [seek, setSeek] = useState(0)
+    const [index, setIndex] = useState(songs.findIndex(s => s.id === activeSong.id))
+    const [seek, setSeek] = useState(0.0)
     const [isSeeking, setIsSeeking] = useState(false)
     const [repeat, setRepeat] = useState(false)
     const [shuffle, setShuffle] = useState(false)
     const [duration, setDuration] = useState(0.0)
     const soundRef = useRef(null)
+    const repeatRef = useRef(repeat)
+    const setActiveSong = useStoreActions((state: any) => state.changeActiveSong)
 
     useEffect(() => {
         let timerId
@@ -47,6 +49,14 @@ export const Player = ({ songs, activeSong }) => {
 
         cancelAnimationFrame(timerId)
     }, [playing, isSeeking])
+
+    useEffect(() => {
+        setActiveSong(songs[index])
+    }, [index, setActiveSong, songs])
+
+    useEffect(() => {
+        repeatRef.current = repeat
+    }, [repeat])
 
     const getIcon = (
         icon: ReactElement<any, string | JSXElementConstructor<any>>,
@@ -106,14 +116,12 @@ export const Player = ({ songs, activeSong }) => {
     }
 
     const onSongEnd = () => {
-        setIndex((state) => {
-            if (repeat) {
-                setSeek(0)
-                soundRef.current.seek(0)
-            } else {
-                nextSong()
-            }
-        })
+        if (repeatRef.current) {
+            setSeek(0)
+            soundRef.current.seek(0)
+        } else {
+            nextSong()
+        }
     }
 
     const onSeek = (e) => {
